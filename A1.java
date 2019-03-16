a7f5690f8f846c6737557a838b51f91d2ed5d6ef
sekhonjashan <36979870+sekhonjashan@users.noreply.github.com>
2019-03-16 13:23:18 -0400

    Run Length Encoding
    PCT Q1 | 16 march

---
+++ A1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class A1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "";
+		int count = 1;
+		int len = 0;
+		Scanner sc = new Scanner(System.in);
+			
+		s = sc.nextLine();
+		s = s + ' ';
+		len = s.length();
+		
+		for(int i = 0 ; i <len-1 ; i++ ) {
+		
+			if( s.charAt(i) == ' ') {
+				break;
+			}
+			
+			if(s.charAt(i) != s.charAt(i+1)) {
+				System.out.print(s.charAt(i));
+				
+			}
+			if(s.charAt(i) == s.charAt(i+1)) {
+				char in = s.charAt(i);
+				for(int j = i+1 ; j <len ; j++ ) {
+					if(in == s.charAt(j)) {
+						count++;
+					}
+					if(in != s.charAt(j)) {
+					break;
+					}
+				}
+			}
+			if (count >1) {
+				System.out.print("("+s.charAt(i)+",");
+				System.out.print(count+")");
+				i = i + count-1;
+				count =1;
+			}
+		}
+	}
+}
